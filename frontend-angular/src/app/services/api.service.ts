724006db62cbd3fe963ff4b745e1452fa89b5924
Durai Manikandan Krishnan <duraimanikandankrishnan@gmail.com>
2025-09-04 23:39:18 +0530

    Docker Integration

---
+++ frontend-angular/src/app/services/api.service.ts
@@ -0,0 +1,13 @@
+// src/app/services/api.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({ providedIn: 'root' })
+export class ApiService {
+  constructor(private http: HttpClient) {}
+  getData(): Observable<any> {
+    return this.http.get('http://localhost:3000/api/data');
+  }
+}
